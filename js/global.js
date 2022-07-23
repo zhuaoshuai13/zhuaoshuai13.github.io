bd89c37af30338f11b69f789941243a98080ea70
zhuaoshuai13 <531024850@qq.com>
2022-07-23 17:03:37 +0800

    Site updated: 2022-07-23 17:03:37

--- js/global.js
+++ js/global.js
@@ -1,13 +1,18 @@
-console.log('aaas')
-const a = document.querySelectorAll('.recent-post-item')
-console.log(a)
-const Wheight = window.innerHeight
+// console.log('aaas')
+// const a = document.querySelectorAll('.recent-post-item')
+// console.log(a);
 
-window.onscroll = function(){
-    for (let i = 0; i < a.length; i++) {
-        if(a[i].getBoundingClientRect().top<Wheight-100){
-            a[i].className = 'recent-post-item animate__zoomIn'
-        }
-    }
-}
+// console.log(a)
+// const Wheight = window.innerHeight
+// window.onload = () => {
+//     window.onscroll = function(){
+//         for (let i = 0; i < a.length; i++) {
+//             if(a[i].getBoundingClientRect().top<Wheight-100){
+//                 a[i].className = 'recent-post-item animate__zoomIn'
+//             }
+//         }
+//     }
+// }
+
+
 
