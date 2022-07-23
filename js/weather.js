93a42f6fd0e45f62eb3ba031fafda0050f9802f1
zhuaoshuai13 <531024850@qq.com>
2022-07-24 01:31:31 +0800

    Site updated: 2022-07-24 01:31:31

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "0124",
+      "background": "5",
+      "tmpColor": "4999DE",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "1",
+      "language": "auto",
+      "borderRadius": "5",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "3a2221f48254431b9f32ca3d0e240533"
+    }
+  }
